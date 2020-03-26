bb435089a9a8c013200bb6494d2854989811323a
mike <chongyang18@gmail.com>
2020-03-27 00:25:04 +0800

    feat(ElementUtil): remove class generics

--- src/main/java/com/lib/utils/ElementUtils.java
+++ src/main/java/com/lib/utils/ElementUtils.java
@@ -1,6 +1,6 @@
 package com.lib.utils;
 
-public class ElementUtils<E> {
+public class ElementUtils {
     public static void swap(int[] numbers, int i, int j) {
         int cur = numbers[i];
         numbers[i] = numbers[j];
