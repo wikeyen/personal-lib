5b832e4dd849256bfa859e49018f5cfdf0ad3288
Mike Yan <chongyang18@gmail.com>
2020-10-13 15:13:19 +0200

    test(topNth): print time cost for findTopNth

--- src/test/java/com/lib/algorithm/sort/TopNTest.java
+++ src/test/java/com/lib/algorithm/sort/TopNTest.java
@@ -12,13 +12,17 @@ class TopNTest {
 
     @Test
     void should_find_the_6th_largest_number_when_findTopNth_given_an_array() {
-        List<Integer> numbers = ElementUtil.getIntegerNumbers(1000, 0, 1000);
-        Collections.sort(numbers);
+        long start1 = System.currentTimeMillis();
+        List<Integer> numbers = ElementUtil.getIntegerNumbers(500000, 0, 1000);
         TopN<Integer> topN = new TopN<>();
-        int rank = 6;
+        int rank = 1000;
 
+        long start2 = System.currentTimeMillis();
         Integer top6thValue = topN.findTopNth(rank - 1, numbers);
+        System.out.println("whole process takes: " + (System.currentTimeMillis() - start1));
+        System.out.println("find top N takes: " + (System.currentTimeMillis() - start2));
 
+        Collections.sort(numbers);
         assertEquals(numbers.get(rank - 1), top6thValue);
     }
 }
--- src/test/java/com/lib/utils/ElementUtil.java
+++ src/test/java/com/lib/utils/ElementUtil.java
@@ -15,7 +15,7 @@ public class ElementUtil {
     }
 
     public static List<Integer> getIntegerNumbers(int amount, int min, int max) {
-        List<Integer> res = new ArrayList<>();
+        List<Integer> res = new ArrayList<>(amount);
 
         for (int i = 0; i < amount; i++) {
             res.add((int) (Math.random() * (max - min) + min));
